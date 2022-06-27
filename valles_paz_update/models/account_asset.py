40407702b8c6f716c8399f8dfbba9783153beec3
eperazaco <eperaza@prointeccr.com>
2022-06-27 16:16:24 -0600

    [FIX] correcion

--- valles_paz_update/models/account_asset.py
+++ valles_paz_update/models/account_asset.py
@@ -15,18 +15,18 @@ class accountAsset(models.Model):
         for asset in self:
             data = asset.depreciation_move_ids.filtered(lambda c: c.state == 'posted')
             if data:
-                self.x_life_of_asset = self.method_number - len(data)
+                asset.x_life_of_asset = asset.method_number - len(data)
                 cont = 0
                 for lines in data:
                     cont += 1
-                    if cont == self.x_life_of_asset:
-                        self.x_depreciation = lines.asset_depreciated_value
-                    if self.x_depreciation:
-                        self.x_actual_value = self.original_value - self.x_depreciation
-                    self.x_depreciation_month = data.amount_total
+                    if cont == asset.x_life_of_asset:
+                        asset.x_depreciation = lines.asset_depreciated_value
+                    if asset.x_depreciation:
+                        asset.x_actual_value = asset.original_value - asset.x_depreciation
+                    asset.x_depreciation_month = data.amount_total
             else:
-                self.x_life_of_asset = self.method_number
-                self.x_depreciation = 0.0
-                self.x_actual_value = self.original_value
-                self.x_depreciation_month = asset.depreciation_move_ids[1].amount_total
+                asset.x_life_of_asset = asset.method_number
+                asset.x_depreciation = 0.0
+                asset.x_actual_value = asset.original_value
+                asset.x_depreciation_month = asset.depreciation_move_ids[1].amount_total
 
